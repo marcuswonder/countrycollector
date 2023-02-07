bd6fbb14a1cd3ffe76831931f51e9d338a38e09b
marcuswonder <marcus.bruce.smith@gmail.com>
2023-02-06 18:52:20 -0800

    Update and Delete functionality added for Countries. End of Django class-based views lab

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('countries/', views.countries_index, name='index'),
     path('countries/<int:country_id>/', views.countries_detail, name='detail'),
+    path('countries/create/', views.CountryCreate.as_view(), name='countries_create'),
+    path('countries/<int:pk>/update/', views.CountryUpdate.as_view(), name='countries_update'),
+    path('countries/<int:pk>/delete/', views.CountryDelete.as_view(), name='countries_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Country
  
 
@@ -17,3 +18,15 @@ def countries_index(request):
 def countries_detail(request, country_id):
   country = Country.objects.get(id=country_id)
   return render(request, 'countries/detail.html', { 'country': country })
+
+class CountryCreate(CreateView):
+  model = Country
+  fields = '__all__'
+
+class CountryUpdate(UpdateView):
+  model = Country
+  fields = '__all__'
+
+class CountryDelete(DeleteView):
+  model = Country
+  success_url = '/countries'
