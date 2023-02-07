f8e11b46f143f66635fcc027a79f70f298d036c1
marcuswonder <marcus.bruce.smith@gmail.com>
2023-02-06 18:32:31 -0800

    Country show page added. Removed hand-coding from index page. End of Django Models lab.

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('countries/', views.countries_index, name='index'),
+    path('countries/<int:country_id>/', views.countries_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -14,3 +14,6 @@ def countries_index(request):
   countries = Country.objects.all()
   return render(request, 'countries/index.html', { 'countries': countries })
 
+def countries_detail(request, country_id):
+  country = Country.objects.get(id=country_id)
+  return render(request, 'countries/detail.html', { 'country': country })
