e0dcefbc920b0ed4385773fc018c58018c86536e
marcuswonder <marcus.bruce.smith@gmail.com>
2023-02-07 17:47:17 -0800

    Database rebuilt and migrations made. Visit model created and tested.

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.urls import reverse
 
+PURPOSES = (
+    ('L', 'Leisure'),
+    ('B', 'Business')
+)
+
 # Create your models here.
 class Country(models.Model):
     name = models.CharField(max_length=100)
@@ -14,3 +19,26 @@ class Country(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'country_id': self.id})
+    
+    class Meta:
+        verbose_name_plural = "countries"
+
+
+class Visit(models.Model):
+    start = models.DateField('Start Date')
+    end = models.DateField('End Date')
+    highlight = models.TextField(max_length=250)
+    roadtrip = models.BooleanField(default=False)
+    purpose = models.CharField(        
+        max_length=1,
+        choices=PURPOSES,
+        default=PURPOSES[0][0]
+    )
+
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_purpose_display()} visit starting on {self.start}"
+
+    class Meta:
+        ordering = ['-start']
