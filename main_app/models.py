b80faf35fc8fddb18bdbc64ec511ac20bdb14a0f
marcuswonder <marcus.bruce.smith@gmail.com>
2023-02-07 17:04:54 -0800

    Reverting to version bd6fbb1 end of lab2

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 from django.urls import reverse
 
-PURPOSE = (
-    ('L', 'Leisure'),
-    ('B', 'Business'),
-)
-
 # Create your models here.
 class Country(models.Model):
     name = models.CharField(max_length=100)
@@ -19,19 +14,3 @@ class Country(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'country_id': self.id})
-
-class Visit(models.Model):
-    start_date = models.DateField()
-    end_date = models.DateField()
-    purpose = models.CharField(
-        max_length=1,
-        choices=PURPOSE,
-        default=PURPOSE[0][0]
-    )
-    highlight = models.TextField(max_length=250)
-    road_trip = models.BooleanField(default=False)
-
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.get_purpose_display} visit starting on {self.start_date}"
